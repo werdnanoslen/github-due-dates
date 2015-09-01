afc531b1ac5fc9cc57faaa59e678d81b156233b8
Andrew Nelson <andy@andyhub.com>
2015-08-31 20:07:00 -0400

    scanned issues for '#due [date]' syntax

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This uses the PyGithub library: ```pip install PyGithub```
 
 ## todo:
 1. ~~get all github issues of all repos of an organization~~ **DONE**
-1. scan issue bodies for "#due [date]."
+1. ~~scan issue bodies for "#due [date]."~~ **DONE**
 1. expose as atom feed
--- thatssofetch.py
+++ thatssofetch.py
@@ -1,11 +1,18 @@
 from github import Github
+import re
 
-g = Github()
+g = Github('19f95493be1225258f1a6edc2aeb62a61292eecc')
 
-org = g.get_organization('')
+org = g.get_organization('codeforatlanta')
 repos = org.get_repos()
-
 for repo in repos:
     issues = repo.get_issues()
     for issue in issues:
-        print(issue.body)
+        body = issue.body
+        hasDueDate = body.find('#due ') > -1
+        if hasDueDate:
+            monthIndexStart = body.find('#due ') + 5
+            monthIndexEnd = body.find('/', monthIndexStart)
+            month = body[monthIndexStart:monthIndexEnd]
+            day = re.search('(/)\w+', body[monthIndexEnd:]).group(0)[1:]
+            print(month, day)
